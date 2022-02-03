7606c861b10c6c83e5e39075af4d5ab12a80efe8
Sunggun Yu <sunggun.dev@gmail.com>
2022-02-02 22:07:03 -0500

    feat: list command

---
+++ cmd/list.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/sunggun-yu/proxy-wrapper/internal/config"
+)
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all profiles",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// current default profile name to compare
+		defaultProfile := viper.GetString(ConfigKeyDefaultProfile)
+
+		// build string array for profiles names to be sorted
+		profiles, err := listProfiles()
+		if err != nil {
+			return err
+		}
+
+		if len(profiles) < 1 {
+			fmt.Println("no profile is existing")
+			return nil
+		}
+
+		// print profiles. mark default profile with *
+		for _, p := range profiles {
+			if p == defaultProfile {
+				fmt.Println("*", p)
+			} else {
+				fmt.Println(" ", p)
+			}
+		}
+		return nil
+	},
+}
+
+// get profile names list
+func listProfiles() ([]string, error) {
+	// unmarshall profile sub section to get keys
+	var c map[string]config.ProxyProfile
+
+	// TODO: set the global var for profile sub section in the init of root command.
+	ps := viper.Sub(ConfigKeyProfile)
+	if ps == nil {
+		return nil, nil
+	}
+
+	if err := ps.Unmarshal(&c); err != nil {
+		return nil, err
+	}
+
+	// build string array for profiles names to be sorted
+	profiles := []string{}
+	for p := range c {
+		profiles = append(profiles, p)
+	}
+	// sort the profiles
+	sort.Strings(profiles)
+	return profiles, nil
+}
