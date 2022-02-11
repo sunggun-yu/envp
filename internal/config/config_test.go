71dc2bb163a07f3d98bdbfc3d044ad01c5573581
Sunggun Yu <sunggun.dev@gmail.com>
2022-02-11 15:00:28 -0500

    fix: categorize test cases

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -68,39 +68,45 @@ func TestEnv(t *testing.T) {
 // ParseEnvFlagToMap should parse string format "env=val" to map "env: val"
 func TestParseEnvFlagToMap(t *testing.T) {
 
-	// nil data test
-	if config.ParseEnvFlagToMap([]string{}) != nil {
-		t.Error("Not meet expectation. empty slice should return nil")
-	}
+	t.Run("when set empty data", func(t *testing.T) {
+		// nil data test
+		if config.ParseEnvFlagToMap([]string{}) != nil {
+			t.Error("Not meet expectation. empty slice should return nil")
+		}
+	})
 
-	testData := testDataArrStringFromFlag
-	expected := testDataEnvMap
-	actual := config.ParseEnvFlagToMap(testData)
+	t.Run("when data exist", func(t *testing.T) {
+		testData := testDataArrStringFromFlag
+		expected := testDataEnvMap
+		actual := config.ParseEnvFlagToMap(testData)
 
-	if !reflect.DeepEqual(expected, actual) {
-		t.Error("Not meet expectation", expected, "-", actual)
-	}
+		if !reflect.DeepEqual(expected, actual) {
+			t.Error("Not meet expectation", expected, "-", actual)
+		}
+	})
 }
 
 // ParseEnvFlagToEnv should parse slice of string "var=val" to []ENV
 func TestParseEnvFlagToEnv(t *testing.T) {
 
-	// nil data test
-	if config.ParseEnvFlagToEnv([]string{}) != nil {
-		t.Error("Not meet expectation. empty slice should return nil")
-	}
+	t.Run("when set empty data", func(t *testing.T) {
+		// nil data test
+		if config.ParseEnvFlagToEnv([]string{}) != nil {
+			t.Error("Not meet expectation. empty slice should return nil")
+		}
+	})
 
-	testData := testDataArrStringFromFlag
-	// invalid format should be ignored without error
-	actual := config.ParseEnvFlagToEnv(testData)
-	expected := testDataEnvs
-	// ParseEnvFlagToEnv sort the result. so expected should be sorted
-	config.SortEnv(expected)
-	if !reflect.DeepEqual(expected, actual) {
-		t.Error("Not meet expectation", expected, "-", actual)
-	}
-
-	// nil test
+	t.Run("when data exist", func(t *testing.T) {
+		testData := testDataArrStringFromFlag
+		// invalid format should be ignored without error
+		actual := config.ParseEnvFlagToEnv(testData)
+		expected := testDataEnvs
+		// ParseEnvFlagToEnv sort the result. so expected should be sorted
+		config.SortEnv(expected)
+		if !reflect.DeepEqual(expected, actual) {
+			t.Error("Not meet expectation", expected, "-", actual)
+		}
+	})
 }
 
 // test MapToEnv func
@@ -117,40 +123,52 @@ func TestMapToEnv(t *testing.T) {
 
 func TestDefaultProfile(t *testing.T) {
 	cfg := testDataConfig()
-	if p, err := cfg.DefaultProfile(); err != nil {
-		t.Error("Should not be nil")
-	} else {
-		fmt.Println(p)
-	}
-	// make default empty
-	cfg.Default = ""
-	if _, err := cfg.DefaultProfile(); err == nil {
-		t.Error("Should be error")
-	} else {
-		fmt.Println(err)
-	}
+
+	t.Run("when default profile exist", func(t *testing.T) {
+		if p, err := cfg.DefaultProfile(); err != nil {
+			t.Error("Should not be nil")
+		} else {
+			fmt.Println(p)
+		}
+	})
+
+	t.Run("when default profile not exist", func(t *testing.T) {
+		// make default empty
+		cfg.Default = ""
+		if _, err := cfg.DefaultProfile(); err == nil {
+			t.Error("Should be error")
+		} else {
+			fmt.Println(err)
+		}
+	})
 }
 
 func TestProfile(t *testing.T) {
 	cfg := testDataConfig()
 
-	if p, err := cfg.Profile(""); err != nil {
-		t.Error("Should not be nil.")
-	} else if p.Desc != "docker" {
-		t.Error("Should not be same as default profile")
-	}
+	t.Run("should return current default when set empty string", func(t *testing.T) {
+		if p, err := cfg.Profile(""); err != nil {
+			t.Error("Should not be nil.")
+		} else if p.Desc != "docker" {
+			t.Error("Should not be same as default profile")
+		}
+	})
 
-	if p, err := cfg.Profile("docker"); err != nil {
-		t.Error("Should not be nil.")
-	} else if p == nil {
-		t.Error("Should not be nil.")
-	}
+	t.Run("normal case", func(t *testing.T) {
+		if p, err := cfg.Profile("docker"); err != nil {
+			t.Error("Should not be nil.")
+		} else if p == nil {
+			t.Error("Should not be nil.")
+		}
+	})
 
-	// make default empty and find profile that is not existing
-	cfg.Default = ""
-	if _, err := cfg.Profile("not-existing-profile"); err == nil {
-		t.Error("Should be error")
-	} else {
-		fmt.Println(err)
-	}
+	t.Run("wehn set non-existing profile", func(t *testing.T) {
+		// make default empty and find profile that is not existing
+		cfg.Default = ""
+		if _, err := cfg.Profile("not-existing-profile"); err == nil {
+			t.Error("Should be error")
+		} else {
+			fmt.Println(err)
+		}
+	})
 }
--- internal/config/profile_test.go
+++ internal/config/profile_test.go
@@ -27,35 +27,41 @@ func TestFindProfileByDotNotationKey(t *testing.T) {
 	testData := testDataConfig()
 	profiles := testData.Profiles
 
-	if p, err := profiles.FindProfile("docker"); p == nil && err != nil {
-		t.Error("Should not be nil and error")
-	} else if p.Desc != "docker" {
-		t.Error("Not meet expectation")
-		fmt.Println(p)
-	}
+	t.Run("find non-nested profile", func(t *testing.T) {
+		if p, err := profiles.FindProfile("docker"); p == nil && err != nil {
+			t.Error("Should not be nil and error")
+		} else if p.Desc != "docker" {
+			t.Error("Not meet expectation")
+			fmt.Println(p)
+		}
+	})
 
-	// happy path
-	if p, err := profiles.FindProfile("org.nprod.argocd.argo2"); p == nil && err != nil {
-		t.Error("Should not be nil and error")
-	} else if p.Desc != "org.nprod.argocd.argo2" {
-		t.Error("Not meet expectation")
-		fmt.Println(p)
-	}
+	t.Run("find nested profile", func(t *testing.T) {
+		if p, err := profiles.FindProfile("org.nprod.argocd.argo2"); p == nil && err != nil {
+			t.Error("Should not be nil and error")
+		} else if p.Desc != "org.nprod.argocd.argo2" {
+			t.Error("Not meet expectation")
+			fmt.Println(p)
+		}
+	})
 
-	// not existing key
-	if p, err := profiles.FindProfile("org.nprod.vault"); p != nil && err == nil {
-		t.Error("Should be nil and err")
-	}
+	t.Run("find non-existing profile", func(t *testing.T) {
+		if p, err := profiles.FindProfile("org.nprod.vault"); p != nil && err == nil {
+			t.Error("Should be nil and err")
+		}
+	})
 
-	// empty string
-	if p, err := profiles.FindProfile(""); p != nil && err == nil {
-		t.Error("Should be nil and err")
-	}
+	t.Run("find with empty string", func(t *testing.T) {
+		if p, err := profiles.FindProfile(""); p != nil && err == nil {
+			t.Error("Should be nil and err")
+		}
+	})
 
-	// wonky format
-	if p, err := profiles.FindProfile(".aaa..aaa"); p != nil && err == nil {
-		t.Error("Should be nil and err")
-	}
+	t.Run("find with wonky format string", func(t *testing.T) {
+		if p, err := profiles.FindProfile(".aaa..aaa"); p != nil && err == nil {
+			t.Error("Should be nil and err")
+		}
+	})
 
 	// pointer check
 	testChangeData := "changed"
@@ -99,20 +105,29 @@ func TestFindParentProfile(t *testing.T) {
 		}
 	}
 
-	// normal case
-	testCaseNormal("lab.cluster1", "lab")
-	// should return parent even child is not exisiting
-	testCaseNormal("lab.cluster-not-exising-in-config", "lab")
+	t.Run("find parent of existing child profile", func(t *testing.T) {
+		// normal case
+		testCaseNormal("lab.cluster1", "lab")
+	})
 
-	// should return nil for non existing profile
-	if p, err := profiles.FindParentProfile("non-exising-profile"); p != nil && err != nil {
-		t.Error("supposed to be nill and no err")
-	}
+	t.Run("find exisiting parent of non-existing child profile", func(t *testing.T) {
+		// should return parent even child is not exisiting
+		testCaseNormal("lab.cluster-not-exising-in-config", "lab")
+	})
 
-	// should return nil for non existing profile
-	if p, err := profiles.FindParentProfile("non-existing-parent.non-existing-child"); p != nil && err == nil {
-		t.Error("supposed to be nil and err")
-	}
+	t.Run("find parent of non-existing profile", func(t *testing.T) {
+		// should return nil for non existing profile
+		if p, err := profiles.FindParentProfile("non-exising-profile"); p != nil && err != nil {
+			t.Error("supposed to be nill and no err")
+		}
+	})
+
+	t.Run("find non-exisiting parent of non-existing child profile", func(t *testing.T) {
+		// should return nil for non existing profile
+		if p, err := profiles.FindParentProfile("non-existing-parent.non-existing-child"); p != nil && err == nil {
+			t.Error("supposed to be nil and err")
+		}
+	})
 }
 
 func TestDeleteProfile(t *testing.T) {
@@ -156,10 +171,18 @@ func TestDeleteProfile(t *testing.T) {
 	}
 
 	// test case for non-nested profile
-	testCase("docker")
-	testCase("lab.cluster1")
-	testCase("org.nprod.argocd.argo2")
-	testCaseNonExistingProfile("non-existing-parent.non-existing-child")
+	t.Run("delete non-nested profile", func(t *testing.T) {
+		testCase("docker")
+	})
+
+	t.Run("delete nested profile", func(t *testing.T) {
+		testCase("lab.cluster1")
+		testCase("org.nprod.argocd.argo2")
+	})
+
+	t.Run("delete non-exisiting nested profile", func(t *testing.T) {
+		testCaseNonExistingProfile("non-existing-parent.non-existing-child")
+	})
 }
 
 // test SetProfile method
@@ -188,12 +211,21 @@ func TestSetProfile(t *testing.T) {
 	}
 
 	// adding non-exising 1st level
-	testCaseNormal("something", "something")
+	t.Run("adding non-exising 1st level", func(t *testing.T) {
+		testCaseNormal("something", "something")
+	})
+
 	// adding into non-exising nested profile
-	testCaseNormal("some.thing", "hello")
+	t.Run("adding into non-exising nested profile", func(t *testing.T) {
+		testCaseNormal("some.thing", "hello")
+	})
+
 	// adding into non-exising nested profile - deeper
-	testCaseNormal("how.about.this.deep.case.meow.meow.woof.woof", "meow")
-	{
+	t.Run("adding into non-exising nested profile - deeper", func(t *testing.T) {
+		testCaseNormal("how.about.this.deep.case.meow.meow.woof.woof", "meow")
+	})
+
+	t.Run("adding into existing nested profile", func(t *testing.T) {
 		// adding into existing nested profile
 		testCaseNormal("org.nprod.argocd.argo100", "argocd")
 		// sibling that was existing before appending should exist after
@@ -201,8 +233,9 @@ func TestSetProfile(t *testing.T) {
 		if s == nil {
 			t.Error("sibling is not exist after appending")
 		}
-	}
-	{
+	})
+
+	t.Run("overwriting existing profile", func(t *testing.T) {
 		// overwriting existing profile
 		testCaseNormal("lab.cluster3", "updated lab.cluster3")
 		// sibling that was existing before appending should exist after
@@ -210,14 +243,21 @@ func TestSetProfile(t *testing.T) {
 		if s == nil {
 			t.Error("sibling is not exist after appending")
 		}
-	}
+	})
 
-	// just to check in human eye
-	out, _ := yaml.Marshal(profiles)
-	fmt.Println(string(out))
+	t.Run("set empty string profile", func(t *testing.T) {
+		err := profiles.SetProfile("", *config.NewProfile())
+		if err == nil {
+			t.Errorf("It supposed to be error")
+		}
+	})
 
-	err := profiles.SetProfile("", *config.NewProfile())
-	if err == nil {
-		t.Errorf("It supposed to be error")
-	}
+	t.Run("marshalling", func(t *testing.T) {
+		out, err := yaml.Marshal(profiles)
+		if err != nil {
+			t.Errorf("marshalling is failed after adding profiles")
+		} else {
+			fmt.Println(string(out))
+		}
+	})
 }
